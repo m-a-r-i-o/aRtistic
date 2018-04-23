a3aa70a31406e50d245e76db1d37ba717576a195
Mario Pasquato <mario.pasquato@gmail.com>
2018-04-23 15:48:47 +0200

    added plotsnap to the functions

--- R/plotsnap.R
+++ R/plotsnap.R
@@ -1,8 +1,17 @@
-plotSnap <- function()
+#' plotSnap function
+#'
+#' This function plots a snapshot in the xy, xz, yz planes
+#' @param file.pro file name of the snapshot, defaults to snap.pro
+#' @param limits plot limits
+#' @keywords plot cluster, plot snapshot, plot, snap, snapshot
+#' @export
+#' @examples
+#' plotSnap(file.pro="thissnap") #plots the snapshot in file thissnap (note that the .pro extension is not needed) with default limits
+
+plotSnap <- function(file.pro="snap.pro", limits=40*c(-1, 1))
 {
-    snap.pro <- read.table("snap.pro", header = T)
-    summary(snap.pro)
-    li <- 40*c(-1, 1)
+    snap.pro <- read.table(file.pro, header = T)
+    li <- limits
     jpeg("xy.jpg")
     plot(snap.pro$x, snap.pro$y, xlim = li, ylim = li, pch = ".", xlab = "x", ylab = "y")
     jpeg("xz.jpg")
