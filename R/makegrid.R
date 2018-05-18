43e0e7cc60ae9be4f0fbcfa1b86070cacf6b1cc1
Mario Pasquato <mario.pasquato@gmail.com>
2018-05-18 14:02:48 +0200

    added paramerter epsi

--- R/makegrid.R
+++ R/makegrid.R
@@ -5,16 +5,17 @@
 #' the number of rows is k that of the original dataframe
 #' @param df numeric dataframe
 #' @param k fill factor
+#' @param epsi generate random numbers in the range [(1-epsi)*min, (1+epsi)*max[
 #' @keywords grid
 #' @export
 #' @examples
 #' makegrid(data.frame(u=rnorm(10),v=rnorm(10)))
 
-makegrid <- function(df, k=1)
+makegrid <- function(df, k=1, epsi = 0.1)
 {
     grid <- apply(df, 2, function(x) {
-    	                      mi <- min(x)
-    	                      ma <- max(x)
+    	                      mi <- (1-epsi)*min(x)
+    	                      ma <- (1+epsi)*max(x)
     	                      l <- k*length(x)
                               return(mi+runif(l)*(ma-mi))
                               })
