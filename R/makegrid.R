d3c27121e99edb87cbe116f882fde24ef01b19b2
Mario Pasquato <mario.pasquato@gmail.com>
2018-05-18 13:47:25 +0200

    introduced fill factor parameter

--- R/makegrid.R
+++ R/makegrid.R
@@ -2,19 +2,20 @@
 #'
 #' This function takes a numeric dataframe and outputs a random grid of
 #' points covering the volume spanned by the dataframe in column space
-#' the number of rows is the same as that of the original dataframe
+#' the number of rows is k that of the original dataframe
 #' @param df numeric dataframe
+#' @param k fill factor
 #' @keywords grid
 #' @export
 #' @examples
 #' makegrid(data.frame(u=rnorm(10),v=rnorm(10)))
 
-makegrid <- function(df)
+makegrid <- function(df, k=1)
 {
     grid <- apply(df, 2, function(x) {
     	                      mi <- min(x)
     	                      ma <- max(x)
-    	                      l <- length(x)
+    	                      l <- k*length(x)
                               return(mi+runif(l)*(ma-mi))
                               })
     return(grid)
