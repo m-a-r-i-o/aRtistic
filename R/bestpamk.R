bd945774f7c7e1003a7f2ac463556ff1096724c9
Mario Pasquato <mario.pasquato@gmail.com>
2018-05-24 11:37:56 +0200

    better output in bestpamk

--- R/bestpamk.R
+++ R/bestpamk.R
@@ -11,5 +11,11 @@
 bestpamk <- function(dataset)
 {
 	library(cluster)
-	return(2)
+	maximum_number_of_clusters <- round(sqrt(nrow(dataset)))
+	minimum_number_of_clusters <- 2
+	if(maximum_number_of_clusters < minimum_number_of_clusters) stop("Too few datapoints for cluster analysis")
+	average_silhouette_width <- function(i) return(pam(dataset, i)$silinfo$avg.width)
+	silwidths <- sapply(minimum_number_of_clusters:maximum_number_of_clusters, average_silhouette_width)
+    output <- list(optimalk = minimum_number_of_clusters+which.max(silwidths)-1, minimum_number_of_clusters = minimum_number_of_clusters, maximum_number_of_clusters = maximum_number_of_clusters, silwidths=silwidths)
+	return(output)
 }
