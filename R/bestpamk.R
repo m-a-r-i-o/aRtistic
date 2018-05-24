b3e30b10609edfa6ca58eb1aeb3a71ef96361be2
Mario Pasquato <mario.pasquato@gmail.com>
2018-05-24 12:06:02 +0200

    now pam standardizes

--- R/bestpamk.R
+++ R/bestpamk.R
@@ -11,10 +11,11 @@
 bestpamk <- function(dataset)
 {
 	library(cluster)
-	maximum_number_of_clusters <- round(sqrt(nrow(dataset)))
+	#k to try: at least 2 clusters, maximum 10 or the sqrt of the number of rows
+	maximum_number_of_clusters <- min(10, round(sqrt(nrow(dataset))))
 	minimum_number_of_clusters <- 2
 	if(maximum_number_of_clusters < minimum_number_of_clusters) stop("Too few datapoints for cluster analysis")
-	average_silhouette_width <- function(i) return(pam(dataset, i)$silinfo$avg.width)
+	average_silhouette_width <- function(i) return(pam(dataset, i, stand = TRUE)$silinfo$avg.width)
 	silwidths <- sapply(minimum_number_of_clusters:maximum_number_of_clusters, average_silhouette_width)
     output <- list(optimalk = minimum_number_of_clusters+which.max(silwidths)-1, minimum_number_of_clusters = minimum_number_of_clusters, maximum_number_of_clusters = maximum_number_of_clusters, silwidths=silwidths)
 	return(output)
